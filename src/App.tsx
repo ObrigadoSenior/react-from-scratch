610a9a77fdfab82a3f2b60621dae9f56d1cf1787
Johan Sjoberg <johansjoberg88@gmail.com>
2022-09-15 16:10:55 +0200

    Added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
+import styled from 'styled-components';
 import { decrement, increment } from './store/slices/template';
 import { useAppDispatch, useAppSelector } from './store/store';
+import GlobalStyle from './globalStyle';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const Button = styled.button`
+  color: white;
+  background-color: #5288c6;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 6px;
+`;
+
+const Value = styled.span`
+  margin: 0 2rem;
+`;
 
 export const App = () => {
   const disaptch = useAppDispatch();
   const { value } = useAppSelector(({ template }) => template);
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => disaptch(decrement())}>Decreament</button>
-        <span>{value}</span>
-        <button onClick={() => disaptch(increment())}>Increament</button>
+    <Container>
+      <GlobalStyle />
+      <header>
+        <Button onClick={() => disaptch(decrement())}>Decrement</Button>
+        <Value>{value}</Value>
+        <Button onClick={() => disaptch(increment())}>Increment</Button>
       </header>
-    </div>
+    </Container>
   );
 };
